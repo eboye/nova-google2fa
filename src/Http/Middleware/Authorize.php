593360f4fc9290d6c5479cb707a5d067743d7767
Vasilj Milošević <eboyee@gmail.com>
2022-04-18 04:19:08 +0200

    Response type

--- src/Http/Middleware/Authorize.php
+++ src/Http/Middleware/Authorize.php
@@ -3,6 +3,7 @@
 namespace Lifeonscreen\Google2fa\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -15,9 +16,9 @@ class Authorize
      *
      * @param Request $request
      * @param Closure $next
-     * @return Response|RedirectResponse
+     * @return Response|RedirectResponse|JsonResponse
      */
-    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
     {
         return resolve(Google2fa::class)->authorize($request) ? $next($request) : abort(403);
     }
