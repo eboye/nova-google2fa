73c49738251361063607fa56d28697f43851a396
Vasilj Milošević <eboyee@gmail.com>
2022-04-18 21:43:33 +0200

    update views

--- resources/views/authenticate.blade.php
+++ resources/views/authenticate.blade.php
@@ -9,7 +9,13 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('app.css', 'vendor/nova') }}">
-
+    <style>
+        @media print {
+            .no-print {
+                display: none;
+            }
+        }
+    </style>
     <script>
         window.checkAutoSubmit = function checkAutoSubmit(el) {
             if (el.value.length === 6) {
@@ -38,7 +44,7 @@
         };
     </script>
 </head>
-<body class="bg-40 text-black h-full">
+<body class="text-gray-500 dark:text-gray-400 bg-gray-100 dark:bg-gray-900 min-h-full">
 <div class="h-full">
     <div class="px-view py-view mx-auto">
         <div class="mx-auto py-8 max-w-sm text-center text-90">
@@ -49,15 +55,15 @@
         <form id="authenticate_form" class="bg-white shadow rounded-lg p-8 max-w-xl mx-auto" method="POST"
               action="/los/2fa/authenticate">
             @csrf
-            <h2 class="p-2">Two Factor Authentication</h2>
+            <h2 class="py-2">Two Factor Authentication</h2>
 
-            <p class="p-2">Two factor authentication (2FA) strengthens access security by requiring two methods (also
+            <p class="py-2">Two factor authentication (2FA) strengthens access security by requiring two methods (also
                 referred to as factors) to
                 verify your identity.
                 Two factor authentication protects against phishing, social engineering and password brute force attacks
                 and secures your logins from attackers
                 exploiting weak or stolen credentials.</p>
-            <p class="p-2"><strong>Enter the pin from your Authenticator app</strong></p>
+            <p class="py-2"><strong>Enter the pin from your Authenticator app</strong></p>
 
             <div class="text-center pt-3">
                 <div class="mb-6 w-1/2" style="display:inline-block">
@@ -69,19 +75,24 @@
 
                     <div id="secret_div">
                         <label class="block font-bold mb-2" for="co">One Time Password</label>
-                        <input class="w-full form-control form-input form-input-bordered" id="secret" type="number"
-                               name="{{ config('google2fa.otp_input') }}" value="" onkeyup="checkAutoSubmit(this)"
-                               placeholder="{{ __('Enter the code...') }}" autofocus="">
+                        <input
+                            class="g-white dark:focus:bg-gray-800 px-4 focus:bg-white focus:outline-none focus:ring appearance-none dark:bg-gray-800 shadow rounded-full h-8 w-full dark:focus:bg-gray-800"
+                            id="secret" type="number"
+                            name="{{ config('google2fa.otp_input') }}" value="" onkeyup="checkAutoSubmit(this)"
+                            placeholder="{{ __('Enter the code...') }}" autofocus="">
                     </div>
 
                     <div id="recover_div" style="display: none;">
                         <label class="block font-bold mb-2" for="co">Recovery code</label>
-                        <input class="w-full form-control form-input form-input-bordered"
-                               placeholder="{{ __('Enter the recovery code...') }}" id="recover" type="text"
-                               name="recover" value="" autofocus="">
+                        <input
+                            class="g-white dark:focus:bg-gray-800 px-4 focus:bg-white focus:outline-none focus:ring appearance-none dark:bg-gray-800 shadow rounded-full h-8 w-full dark:focus:bg-gray-800"
+                            placeholder="{{ __('Enter the recovery code...') }}" id="recover" type="text"
+                            name="recover" value="" autofocus="">
                     </div>
                 </div>
-                <button class="mb-6 w-1/2 btn btn-default btn-primary hover:bg-primary-dark" type="submit">
+                <button
+                    class="mb-6 mx-auto no-print shadow rounded focus:outline-none focus:ring bg-primary-500 hover:bg-primary-400 active:bg-primary-600 text-white dark:text-gray-800 flex items-center font-bold px-4 h-9 text-sm flex-shrink-0"
+                    type="submit">
                     Authenticate
                 </button>
                 <div>
