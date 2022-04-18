3fd042e314c4f02f0c8cbd044ae6997385938443
Vasilj Milošević <eboyee@gmail.com>
2022-04-18 04:20:10 +0200

    Response type

--- src/Http/Middleware/Google2fa.php
+++ src/Http/Middleware/Google2fa.php
@@ -3,6 +3,7 @@
 namespace Lifeonscreen\Google2fa\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -24,12 +25,12 @@ class Google2fa
      *
      * @param Request $request
      * @param Closure $next
-     * @return Response|RedirectResponse
+     * @return Response|RedirectResponse|JsonResponse
      * @throws IncompatibleWithGoogleAuthenticatorException
      * @throws InvalidCharactersException
      * @throws SecretKeyTooShortException
      */
-    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
     {
         if (!config('lifeonscreen2fa.enabled')) {
             return $next($request);
